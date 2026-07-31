c3d7481d4a0e1c3bf2dcaaf5630189bc23eda8f8
vivekds19 <dsvivek092@gmail.com>
2026-07-31 11:12:38 +0530

    Implemented the account number validation

--- src/com/iispl/validations/AccountNumberValidation.java
+++ src/com/iispl/validations/AccountNumberValidation.java
@@ -1,5 +1,22 @@
 package com.iispl.validations;
 
-public class AccountNumberValidation {
+import com.iispl.enums.ChequeValidationEnum;
+import com.iispl.model.Cheque;
+
+public class AccountNumberValidation implements ValidationRule {
+
+	    @Override
+	    public ChequeValidationEnum validate(Cheque cheque) {
+
+	        if (cheque.getAccountNumber() == null || cheque.getAccountNumber().trim().isEmpty()) {
+	            return ChequeValidationEnum.INVALID_ACCOUNT_NUMBER;
+	        }
+
+	        if (Integer.parseInt(cheque.getAccountNumber()) > 0) {
+	            return ChequeValidationEnum.VALIDATION_SUCCESS;
+	        }
+
+	        return ChequeValidationEnum.INVALID_ACCOUNT_NUMBER;
+	    }
 
 }
