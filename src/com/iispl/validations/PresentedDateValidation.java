63452ecca092dfd06c4501a6277b97e8d860b44c
saijagan724 <saijagan724@gmail.com>
2026-07-31 11:16:14 +0530

    Implemented Presented Date Validation

--- src/com/iispl/validations/PresentedDateValidation.java
+++ src/com/iispl/validations/PresentedDateValidation.java
@@ -1,5 +1,21 @@
 package com.iispl.validations;
 
-public class PresentedDateValidation {
+import java.time.LocalDate;
 
+import com.iispl.enums.ChequeValidationEnum;
+import com.iispl.model.Cheque;
+
+public class PresentedDateValidation implements ValidationRule{
+
+	@Override
+	public ChequeValidationEnum validate(Cheque cheque) {
+		
+		if(cheque.getPresentedDate().compareTo(cheque.getChequeDate())<0||cheque.getPresentedDate().compareTo(LocalDate.now())>0) {
+			return ChequeValidationEnum.INVALID_PRESENTED_DATE;
+		}else {
+			return ChequeValidationEnum.VALIDATION_SUCCESS;
+		}
+		
+	}
+	
 }
