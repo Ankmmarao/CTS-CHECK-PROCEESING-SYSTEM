4291b24e160f1383907cfd530a6d18e55be0dae6
venkataprasad-14 <venkataprasad1422@gmail.com>
2026-08-03 12:21:08 +0530

    Added sortByClearingZoneAndAmount method

--- src/com/iispl/services/ChequeService.java
+++ src/com/iispl/services/ChequeService.java
@@ -28,7 +28,7 @@ public interface ChequeService {
 
         void checkProcessingReports();
     
-
+        void sortByClearingZoneAndAmount(List<Cheque> eligibleChequeList);
     /*void sortByChequeByPresentingBankAndAmount(List<Cheque> chequeList);
 
 	void sortChequeByDate(List<Cheque> allCheques);
