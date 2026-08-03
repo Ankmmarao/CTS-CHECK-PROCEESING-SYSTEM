5e6e9cf5332a1865b5ea8b5c39c0d419e117fc05
venkataprasad-14 <venkataprasad1422@gmail.com>
2026-08-03 10:58:45 +0530

    not displaying cheque that doesnot have account number

--- src/com/iispl/services/ChequeServiceImpl.java
+++ src/com/iispl/services/ChequeServiceImpl.java
@@ -51,7 +51,7 @@ public class ChequeServiceImpl implements ChequeService {
 
         for (Cheque cheque : chequeList) {
         	
-        	if(cheque.getChequeNumber()!=null || cheque.getPresentingBank()!=null || cheque.getDrawerName()!=null) {
+        	if(cheque.getChequeNumber()!=null || cheque.getPresentingBank()!=null || cheque.getDrawerName()!=null || cheque.getAccountNumber()!=null) {
         		 System.out.println(cheque);
         	}
            
